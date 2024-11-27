5ca2857d3485d421474c5e015fb6387a91e7a769
Fernando Yaguana <109767537+FernandoYaguana@users.noreply.github.com>
2024-11-27 16:31:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hola Mundo</title>
+    <title>Hola Mundo - EC2</title>
 </head>
 <body>
-    <h1>Hola Mundo desde <?php echo gethostname(); ?></h1>
+    <h1>Hola Mundo desde EC2</h1>
+    <p>Esta página se sirve desde la instancia EC2: <strong><?php echo gethostname(); ?></strong></p>
 </body>
 </html>
